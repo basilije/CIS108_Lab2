3cbbe50f9c305e8ad6d0336ea03d262658e61290
basilije <basilije@users.noreply.github.com>
2019-01-30 10:41:12 -0800

    Update Exercise1.cpp

--- Exercise1/Exercise1.cpp
+++ Exercise1/Exercise1.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main()
 {
-	//entering an age
+	// entering an age
 	cout << "Enter your age: "; 
 	int age;
 	cin >> age;
 
-	int age_after_ten_years = age + 10; //calculating an age in ten years
-	cout << "In ten years you will be " << age_after_ten_years << " old." << endl; //printing an age in ten years
+	int age_after_ten_years = age + 10; // calculating an age in ten years
+	cout << "In ten years you will be " << age_after_ten_years << " old." << endl; // printing an age in ten years
 
-	//entering a fahrenheit temperature
+	// entering a fahrenheit temperature
 	cout << "Enter temperature in degrees in fahrenheit: "; 
 	double fahrenheit_temperature;
 	cin >> fahrenheit_temperature;
 
-	double celsius_degree_temperature = (fahrenheit_temperature - 32) * 5 / 9; //calculating a celsius temperature
-	cout << "The temperature in degrees celsius is " << celsius_degree_temperature << endl; //printing a celsius temperature
+	double celsius_degree_temperature = (fahrenheit_temperature - 32) * 5 / 9; // calculating a celsius temperature
+	cout << "The temperature in degrees celsius is " << celsius_degree_temperature << endl; // printing a celsius temperature
 
 	return 0;
 }
